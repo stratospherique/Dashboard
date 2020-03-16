389a64b119208a5c6c32bd6549d9afecd4b17bfe
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2020-03-16 19:44:30 +0100

    add styled components and font awesome packages

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { MainContainer } from './styled/containers';
+import Header from './header';
 
 
 const App = () => {
   return (
-    <h1>Heelo dashboard</h1>
+    <MainContainer>
+      <Header />
+      <h1>Heelo dashboard</h1>
+    </MainContainer>
   );
 }
 
---
+++ src/components/styled/containers.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  place-content: center;
+`;
+
+export { MainContainer };
